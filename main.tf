224306ce3aff2dee31abe3b368316c1a0af5d442
Bruce Becker <brucellino@protonmail.com>
2022-09-11 17:36:04 +0200

    fix: make project depend explicitly on vpc
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
+# The VPC is usally used as a container to include droplets and other resources into.
+# These resources can be added to the project later, but the VPC itself cannot.
+# We therefore make the project depend on the creation of the VPC, since when it comes
+# time to destroy the VPC, we first want all of the resources in it destroyed, before
+# we proceed to destroy the project itself.
 resource "digitalocean_project" "p" {
+  depends_on  = [digitalocean_vpc.vpc]
   name        = var.project.name
   description = var.project.description
   purpose     = var.project.purpose
